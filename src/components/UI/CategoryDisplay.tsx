c1d7350141bd17cb56d7050e80f473ff72bed4a8
gandib <55914763+gandib@users.noreply.github.com>
2025-01-07 23:05:23 +0600

    Category updated

--- src/components/UI/CategoryDisplay.tsx
+++ src/components/UI/CategoryDisplay.tsx
@@ -18,21 +18,23 @@ const CategoryDisplay = ({ category }: { category: ICategory[] }) => {
 
       <div className="flex gap-4 overflow-x-auto scrollbar">
         {category?.map((cat) => (
-          <div key={cat?.id} className="w-30 p-4">
-            <Card
-              key={cat?.id}
-              onPress={() => handleCategory(cat?.name)}
-              className="flex flex-col items-center cursor-pointer p-4 rounded-lg shadow hover:bg-gray-100 transition w-[150px] h-[150px]"
-            >
-              <Image
-                src={cat?.img}
-                width={200}
-                height={200}
-                alt={cat?.name}
-                className="rounded-full  h-full"
-              />
-              <p className="mt-2 text-sm font-medium">{cat?.name}</p>
-            </Card>
+          <div
+            onClick={() => handleCategory(cat?.name)}
+            key={cat?.id}
+            className="w-30 p-4"
+          >
+            <div>
+              <Card className="flex flex-col items-center cursor-pointer p-4 rounded-lg shadow hover:bg-gray-100 transition w-[150px] h-[150px]">
+                <Image
+                  src={cat?.img}
+                  width={200}
+                  height={200}
+                  alt={cat?.name}
+                  className="rounded-full h-full"
+                />
+                <p className="mt-2 text-sm font-medium">{cat?.name}</p>
+              </Card>
+            </div>
           </div>
         ))}
       </div>
